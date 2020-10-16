2306856e3eafd48c4a53ca4b6679549efea53d67
farfotfir@gmail.com <fatemeasadi.1033@yahoo.com>
2020-10-16 20:44:12 +0330

    add main fragment.

---
+++ app/src/main/java/com/example/taskmanagerkanban/controller/fragment/MainFragment.java
@@ -0,0 +1,35 @@
+package com.example.taskmanagerkanban.controller.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.taskmanagerkanban.R;
+
+
+public class MainFragment extends Fragment {
+
+
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view= inflater.inflate(R.layout.fragment_main, container, false);
+        return view;
+    }
+}
