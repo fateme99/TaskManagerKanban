0520e82f585654f6da125ba2df60fac1cabd87e8
farfotfir@gmail.com <fatemeasadi.1033@yahoo.com>
2020-10-17 14:03:32 +0330

    add TaskDBHelper in database.

--- app/src/main/java/com/example/taskmanagerkanban/database/TaskDBHelper.java
+++ app/src/main/java/com/example/taskmanagerkanban/database/TaskDBHelper.java
@@ -1,4 +1,31 @@
 package com.example.taskmanagerkanban.database;
 
-public class TaskDBHelper {
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import com.example.taskmanagerkanban.database.DatabaseSchema.TaskTable.TaskCols;
+import androidx.annotation.Nullable;
+
+public class TaskDBHelper extends SQLiteOpenHelper {
+    public TaskDBHelper(@Nullable Context context) {
+        super(context, DatabaseSchema.NAME, null, DatabaseSchema.VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+        StringBuilder sb=new StringBuilder();
+        sb.append("CREATE TABLE "+ DatabaseSchema.TaskTable.NAME + "( ");
+        sb.append(TaskCols.ID + " INTEGER PRIMARY KEY AUTOINCREMENT , ");
+        sb.append(TaskCols.TITLE + " TEXT , ");
+        sb.append(TaskCols.DESCRIPTION + " TEXT , ");
+        sb.append(TaskCols.DATE + " TEXT ,");
+        sb.append(TaskCols.CLOCK + " TEXT ");
+        sb.append(");");
+        sqLiteDatabase.execSQL(sb.toString());
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
 }
