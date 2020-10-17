c186a105c432e154597e003e5c342de6e90730e5
farfotfir@gmail.com <fatemeasadi.1033@yahoo.com>
2020-10-17 13:37:27 +0330

    add tab to MainActivity in top of page.

--- app/src/main/java/com/example/taskmanagerkanban/controller/activity/MainActivity.java
+++ app/src/main/java/com/example/taskmanagerkanban/controller/activity/MainActivity.java
@@ -4,25 +4,24 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentActivity;
-import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager2.adapter.FragmentStateAdapter;
 import androidx.viewpager2.widget.ViewPager2;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.widget.TableLayout;
 
 import com.example.taskmanagerkanban.R;
 import com.example.taskmanagerkanban.controller.fragment.MainFragment;
-import com.example.taskmanagerkanban.model.Task;
 import com.example.taskmanagerkanban.utils.TaskState;
-
-import java.util.List;
+import com.google.android.material.tabs.TabItem;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 public class MainActivity extends AppCompatActivity {
-    private TableLayout mTableLayout;
+    private TabLayout mTabLayout;
     private ViewPager2 mPager2;
+
     public Intent newIntent(Context context){
         Intent intent=new Intent(context,MainActivity.class);
         return intent;
@@ -36,13 +35,20 @@ public class MainActivity extends AppCompatActivity {
         initView();
     }
     private void findViews(){
-        //mTableLayout=findViewById(R.id.tab);
+        mTabLayout =findViewById(R.id.tab);
         mPager2=findViewById(R.id.viewPager);
+
     }
     private void initView(){
         TaskAdapter taskAdapter=new TaskAdapter(this,TaskState.TODO);
         mPager2.setAdapter(taskAdapter);
+        manageTab();
+
+
     }
+
+
+
     private class TaskAdapter extends FragmentStateAdapter{
         TaskState mTaskState;
 
@@ -58,6 +64,7 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public Fragment createFragment(int position) {
             MainFragment mainFragment=MainFragment.newInstance(mTaskState);
+
             return mainFragment;
         }
 
@@ -65,5 +72,30 @@ public class MainActivity extends AppCompatActivity {
         public int getItemCount() {
             return 3;
         }
+
+
+
+    }
+    private void manageTab() {
+        TabLayoutMediator tabLayoutMediator=new TabLayoutMediator(
+                mTabLayout,
+                mPager2,
+                new TabLayoutMediator.TabConfigurationStrategy() {
+                    @Override
+                    public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+                        switch (position){
+                            case 0:
+                                tab.setText("TO DO");
+                                break;
+                            case 1:
+                                tab.setText("DOING");
+                                break;
+                            case 2:
+                                tab.setText("DONE");
+
+                        }
+                    }
+                });
+        tabLayoutMediator.attach();
     }
 }
